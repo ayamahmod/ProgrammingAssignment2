e000e33bfbff09662713b3d37aa72d5699d5e260
Aya <yoyomahmoud93@gmail.com>
2015-10-25 22:09:33 +0200

    Revert "Ass1"
    This reverts commit 73ec380845a73d768fb794f70d389aaa643b99f9.

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,29 @@
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
-        x <<- y #x is the matrix
-        m <<- NULL # m is the inverse of the matrix
+        x <<- y
+        m <<- NULL
     }
-    get <- function() x #return the matrix
-    setInv <- function(Inv) m <<- Inv #set inverse of the matrix 
-    getInv <- function() m #return inverse of the matrix
-    list(set = set, get = get,      #list of the function in makeCacheMatrix
+    get <- function() x
+    setInv <- function(Inv) m <<- Inv
+    getInv <- function() m
+    list(set = set, get = get,
          setInv = setInv,
          getInv = getInv)
 }
 
 
-## Calculate the inverse of the matrix and cache it
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getInv() #check if inverse ic calculated or not
+    m <- x$getInv()
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
-    data <- x$get() #get the matrix
-    m <- solve(data) #calculate the inverse
-    x$setInv(m) #cache the inverse
-    m #return it
+    data <- x$get()
+    m <- solve(data)
+    x$setInv(m)
+    m
 }
